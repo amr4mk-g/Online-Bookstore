3238f5e4bb454e4ef10ab76d54c8e7d4079941f3
Andrew Essam <andrewessam-2007@hotmail.com>
2023-08-02 18:59:50 +0300

    uploaded by Andrew

--- backend/nodeAPI/src/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const data = ""
-const URL = `mongodb+srv://${data}@cluster0.yqhwxuy.mongodb.net/OnlineBooks?retryWrites=true&w=majority`;
-
-mongoose.connect(URL, {useNewUrlParser: true, useUnifiedTopology: true})
-  .then(() => console.log('Connected to MongoDB'))
-  .catch((err) => console.error('Error connecting to MongoDB:', err));
- 
